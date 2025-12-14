aa4a1093e496775b546e0f61fb4a6421588af1c3
John <119649188+ArmadaDev25@users.noreply.github.com>
2025-12-14 15:51:24 -0700

    Added jQuery and aswapped over the document ready scripting

--- home.php
+++ home.php
@@ -6,9 +6,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <!--JQuery Include-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
         // Runs the update time function upon the loading of the webpage
-        document.addEventListener('DOMContentLoaded', function() {
+        $(document).ready(function() {
             function updateTime(){
                 // gets the current time
                 var currentTime = new Date();
