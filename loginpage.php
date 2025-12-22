f3bd1833f10d0b2834facda104fe351c6be29ecd
John <119649188+ArmadaDev25@users.noreply.github.com>
2025-12-21 18:05:09 -0700

    Login Now Working

--- loginpage.php
+++ loginpage.php
@@ -1,3 +1,48 @@
+<?php
+// Starts the session for this page
+session_start();
+
+// Message that displays on when the login fails
+$message = "";
+
+// Login Logic
+// Check to see if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //Include the database connection file here
+    require_once 'dbconnect.php';
+    // Variables 
+    // username variable
+    $username = $_POST['txtUserName'];
+    // variable to hold the password
+    $password = $_POST['txtPassword'];
+    // Query the database for a match for the username and password
+    $sql = "SELECT user, accountID FROM accounts WHERE username = '$username' AND password = '$password'";
+    // run the query
+    $result = mysqli_query($db_conn, $sql);
+
+    // convert the query result into an array
+    $arrUser = mysqli_fetch_array($result);
+
+    // check to see if the user logged in correctly
+    if (isset($arrUser)){
+        $message = "Login Sucess";
+
+        // set up a session variable to inicate that the user has logged in
+        $_SESSION['username'] = $username;
+
+
+        // Redirect to the intranet page
+        header("Location: dashboard.php");
+
+    }else{
+        $message = "Login Failed";
+        $_SESSION['username'] = "";
+
+    }
+
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -5,6 +50,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <!-- Link to Global CSS file -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -12,11 +59,10 @@
         <h1>Login</h1>
     </header>
     <main>
-        <!--Div Message that is displayed if the data in the form is not valid-->
-        <div id="errorMessage"></div>
+        <p> <?php echo $message;?></p>
         
         
-        <form name="frmLogin" id="frmLogin" method="post" action="">
+        <form name="frmLogin" id="frmLogin" method="post" action="loginpage.php">
             <!-- username input-->
             <div>
                 <label for="txtUserName">Username:</label>
@@ -27,7 +73,7 @@
                 <label for="txtPassword">Password:</label>
                 <input type="password" id="txtPassword" name="txtPassword">
             </div>
-            <!-- password input-->
+            <!-- Submit Button-->
             <button type="submit">Submit</button>
             
 
--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <nav>
 <?php
-    echo "<a href='home.php'>Home</a> <a href='loginpage.php'>Login</a>"
+    echo "<a href='home.php'>Home</a> <a href='loginpage.php'>Login</a> <a href='dashboard.php'>My Dashboard</a>"
 
 ?>
 </nav>
